606b92761da76960498ead1a9bb23f7542ff879d
JudahSan <61739631+JudahSan@users.noreply.github.com>
2025-09-24 16:35:11 +0000

    Deploying to gh-pages from @ JudahSan/judahsan.github.io@9a6672ca7597ad69adb532270526d7d1fc80fc2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -264,6 +264,13 @@ ninja.data = [{
           window.open("https://leetcode.com/u/judahsan/", "_blank");
         },
       },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
